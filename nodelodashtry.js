0ca711055d2a2bc4314e0a50e11be5e057e8b1e1
cicimarisa <cicimarisahardianti@gmail.com>
2017-07-28 13:00:44 +0700

    Changes more

--- nodelodashtry.js
+++ nodelodashtry.js
@@ -1,8 +1,9 @@
-var lodash = require("lodash");
+//var lodash = require("lodash");
 
 //var output = lodash.without([1,2,3],1);
 //console.log(output);
 
+var colors = require("colors");
 exports.printMsg = function(){
-	console.log("this is message from npm demo-page");
+	console.log("this is message from npm demo-page".red);
 }
