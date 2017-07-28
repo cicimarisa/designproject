718ba87a68a62c3a2c64fe152c72e961a15a78bd
cicimarisa <cicimarisahardianti@gmail.com>
2017-07-28 12:09:46 +0700

    Changes

---
+++ nodelodashtry.js
@@ -0,0 +1,8 @@
+var lodash = require("lodash");
+
+//var output = lodash.without([1,2,3],1);
+//console.log(output);
+
+exports.printMsg = function(){
+	console.log("this is message from npm demo-page");
+};
